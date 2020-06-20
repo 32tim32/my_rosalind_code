04d7fdded4820d28a8f9a8b72a52d53b81003795
unknown <tnieuwe1@jhmi.edu>
2020-06-20 10:25:40 -0400

    Ran and described the top answer from Rosalind.

--- SUBS_test/SUBS.py
+++ SUBS_test/SUBS.py
@@ -12,10 +12,18 @@ for nuc in dna:
     if nuc == motif[0]:
         if motif == dna[indx:(indx+mot_len)]:
             mot_found.append(indx + 1)
-
-
+            
     indx += 1
     continue
-
+#The * is used to unpack and make the call iterate 
 print(*mot_found, sep =' ')
+
+#Example from Rosalind by Leandro Lima
+#Turned my lines 1-5 into one line
+s1,s2 = open('rosalind_subs.txt').read().split('\r\n')
+#For index that spans the length of the entire question
+for i in range(len(s1)):
+    #If this index startswith the motif then print the index + 1
+    if s1[i:].startswith(s2):
+        print(i+1,)
  
